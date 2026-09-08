002f825752dc4929f6f6a50713ce0864b245c6ce
rodkevproxy <kevinrodasuk@gmail.com>
2026-09-08 16:17:12 +0100

    Add initial implementation of Queue data structure in Queues.py

---
+++ Queues.py
@@ -0,0 +1,7 @@
+#Queue = FIFO data structure. First In, First Out 
+#       A collection designed for holding elements prior to processing 
+#       Linear data structure 
+
+#       add = enque, 
+#       remove = dequeue 
+
