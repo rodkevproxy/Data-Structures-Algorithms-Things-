2e553bdeca83b4f636491a14218318c0a269bc16
rodkevproxy <kevinrodasuk@gmail.com>
2026-09-08 15:00:39 +0100

    Refactor main function to improve readability and structure in Stacks.py

--- Stacks.py
+++ Stacks.py
@@ -3,12 +3,21 @@
 #       Push() toadd to the top 
 #       Pop() to remove from the top 
 
-def main(): 
+def main():
+    stack = [] 
+    stack.append("Minecraft")
+    stack.append("Doom")
+    stack.append("Fortnite")
+    print(stack)
 
-
-
+    stack.pop()
+    print(stack)
 
 
 if __name__ == '__main__': 
-    main()
-    
+        main()
+
+
+
+
+
