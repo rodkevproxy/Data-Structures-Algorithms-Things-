40b36d005941dd9cda6da546b38016b5343d958d
rodkevproxy <kevinrodasuk@gmail.com>
2026-09-09 23:19:34 +0100

    Remove Python implementations of Queue and Stack data structures; add initial Java class for Stack

--- Queues.py
+++
@@ -1,7 +0,0 @@
-#Queue = FIFO data structure. First In, First Out 
-#       A collection designed for holding elements prior to processing 
-#       Linear data structure 
-
-#       add = enque, 
-#       remove = dequeue 
-
