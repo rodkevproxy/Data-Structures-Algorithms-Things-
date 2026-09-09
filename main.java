6d916893115697e1bf3004c98e7d2867a535a793
rodkevproxy <kevinrodasuk@gmail.com>
2026-09-09 23:22:53 +0100

    Add initial Java implementation of main class with Stack instantiation

---
+++ main.java
@@ -0,0 +1,10 @@
+import java.util.Stack;
+
+public class main {
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<String>(); 
+        
+        
+    }
+    
+}
