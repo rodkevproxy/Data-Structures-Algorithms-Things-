831cbd5e906d58c778a593792d19fad74c85fc54
rodkevproxy <kevinrodasuk@gmail.com>
2026-09-10 22:26:33 +0100

    Refactor main method in stacks.java to enhance stack operations and output

--- stacks.java
+++ stacks.java
@@ -5,7 +5,15 @@ public class stacks {
         Stack<String> stack = new Stack<String>();
 
         //System.out.println(stack.empty()); This is how we check if a stack is empty 
-        
+        stack.push("Minecraft"); 
+        stack.push("Doom"); 
+        stack.push("Wardogs"); 
+        stack.push("Any other game tbh"); 
+
+        System.out.println(stack);
+
+
+
  
 
 
