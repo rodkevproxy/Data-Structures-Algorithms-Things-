9a72108281a4de0e7cdcbbd4c666b3fb40d5315d
rodkevproxy <kevinrodasuk@gmail.com>
2026-09-10 22:29:01 +0100

    Add pop operation to main method in stacks.java for stack manipulation

--- stacks.java
+++ stacks.java
@@ -12,6 +12,16 @@ public class stacks {
 
         System.out.println(stack);
 
+        stack.pop(); //This will remove the top object, when doing this proccess, if you just want to re-assing it can be done following these steps 
+        String popGame = stack.pop(); 
+        System.out.println(popGame);
+
+        
+
+
+
+
+
 
 
  
