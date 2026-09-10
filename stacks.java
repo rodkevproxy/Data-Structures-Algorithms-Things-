c9118807918d597626e85b7e8eff0777ffe25016
rodkevproxy <kevinrodasuk@gmail.com>
2026-09-10 22:11:29 +0100

    Refactor main method in stacks.java to improve readability

--- stacks.java
+++ stacks.java
@@ -2,7 +2,13 @@ import java.util.Stack;
 
 public class stacks {
     public static void main(String[] args) {
-        Stack<String> stack = new Stack<String>(); 
+        Stack<String> stack = new Stack<String>();
+
+        //System.out.println(stack.empty()); This is how we check if a stack is empty 
+        
+ 
+
+
         
         
     }
