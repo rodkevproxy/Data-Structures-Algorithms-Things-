fbed2591acffd0e46c0e3255b5be9950589eb37e
rodkevproxy <kevinrodasuk@gmail.com>
2026-09-11 22:34:16 +0100

    Add comments to queues.java for clarity on queue operations and structure

--- queues.java
+++ queues.java
@@ -1,3 +1,10 @@
+// Queue = FiFo data structure. A collection designed for holding elements prior to procesing. Linear data structure 
+
+// Add = Enqueue, offer()
+// Remove = dequeue, poll()
+
+
+
 public class queues {
     
 }
