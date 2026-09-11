8fd9cc51e849a764e9080c6417a08075c09621f8
rodkevproxy <kevinrodasuk@gmail.com>
2026-09-11 22:26:33 +0100

    Fix typo in comment for memory demonstration in stacks.java

--- stacks.java
+++ stacks.java
@@ -18,9 +18,13 @@ public class stacks {
         System.out.println(stack.search("Doom")); //This will return the position of that object that we want to find 
 
 
-        for(int i = 0; i < 1000; i++) {     //This example demostrates that Java can alro run out of memory 
+        for(int i = 0; i < 1000; i++) {     //This example demostrates that Java can also run out of memory 
             stack.push("Fallout 76"); 
          }
+
+         
+
+
 
 
 
