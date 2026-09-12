7c971b302ff7ecc44968e8501a6dea0069173283
rodkevproxy <kevinrodasuk@gmail.com>
2026-09-12 15:28:54 +0100

    Fix typo in comment for dequeue operation in queues.java

--- queues.java
+++ queues.java
@@ -1,10 +1,17 @@
 // Queue = FiFo data structure. A collection designed for holding elements prior to procesing. Linear data structure 
 
 // Add = Enqueue, offer()
-// Remove = dequeue, poll()
+// Remove = Dequeue, poll()
 
-
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class queues {
-    //queues
+    public static void main(String[] args) {
+        Queue<String> queue = new LinkedList<String>(); //Queue is an interface and not a class, we can not create an instance of an interface. We are creating a LinkedList because this class implements queues 
+
+
+
+        
+    }
 }
