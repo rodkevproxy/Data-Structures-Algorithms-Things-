068ffb1bf7773bfdf85e33cafd0c84e4aa257cfa
rodkevproxy <kevinrodasuk@gmail.com>
2026-09-12 20:39:20 +0100

    Refactor main method in queues class for improved readability and structure

--- queues.java
+++ queues.java
@@ -8,7 +8,19 @@ import java.util.Queue;
 
 public class queues {
     public static void main(String[] args) {
+    
         Queue<String> queue = new LinkedList<String>(); //Queue is an interface and not a class, we can not create an instance of an interface. We are creating a LinkedList because this class implements queues 
+        queue.offer("Karen"); 
+        queue.offer("Chad"); 
+        queue.offer("Steve"); 
+        queue.offer("Harold");
+        System.out.println(queue);
+        System.out.println(queue.peek());
+        queue.poll(); 
+        System.out.println(queue);
+        
+        /.
+        
 
 
 
