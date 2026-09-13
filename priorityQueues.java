78f3dca141089c9a2abe811635e7f14f2c7073ad
rodkevproxy <kevinrodasuk@gmail.com>
2026-09-13 19:58:52 +0100

    Add initial implementation of priorityQueues class with main method and comments

---
+++ priorityQueues.java
@@ -0,0 +1,12 @@
+import java.util.*; 
+
+public class priorityQueues {
+    
+    public static void main(String[] args) {
+         //Priory Queue = A FIFO data structure that serves elements 
+         //                with the highest priorities first 
+         //                  before elements with lower priority
+
+    }
+    
+}
