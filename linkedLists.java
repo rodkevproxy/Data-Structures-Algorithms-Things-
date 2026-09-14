1bcdf48c06a040e4be11bf9d3e62b5927d1a2c33
rodkevproxy <kevinrodasuk@gmail.com>
2026-09-14 21:36:17 +0100

    Update comments in main method of linkedLists class for accuracy and clarity

--- linkedLists.java
+++ linkedLists.java
@@ -2,8 +2,10 @@
 
 class linkedLists{ 
         public static void main(String[] args) {
-                    //Linked list, singly 
-                    //It has a node to the next element 
+                    //Linked list, singly and doubly (This one stores two addresses)
+                    //It has a node to the next element, each node has a address. 
+                    //LinkedList are bad are searching 
+
 
         }
 }
