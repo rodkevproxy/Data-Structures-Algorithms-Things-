1e71599bd5ad2848e514d4e6eb8036ab988da053
rodkevproxy <kevinrodasuk@gmail.com>
2026-09-14 21:49:29 +0100

    Fix typo in comment about LinkedList search performance

--- linkedLists.java
+++ linkedLists.java
@@ -1,10 +1,13 @@
-
+import java.util.LinkedList;
 
 class linkedLists{ 
         public static void main(String[] args) {
                     //Linked list, singly and doubly (This one stores two addresses)
                     //It has a node to the next element, each node has a address. 
-                    //LinkedList are bad are searching 
+                    //LinkedList are bad at searching 
+
+                    LinkedList<String> linkedList = new LinkedList<>(); 
+                    
 
 
         }
