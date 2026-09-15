0a3641d84ab0922cb00e5573563d58e0b506bd36
rodkevproxy <kevinrodasuk@gmail.com>
2026-09-15 22:49:25 +0100

    Remove unnecessary blank lines in dynamicArray class

--- dynamicArray.java
+++ dynamicArray.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 public class dynamicArray {
         public static void main(String[] args) {
             ArrayList<String> arrayList = new ArrayList<>(); 
+
+
             
         }    
 }
