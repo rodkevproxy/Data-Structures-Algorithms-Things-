e2457e42bb613924456481f47e0cb2da984447da
rodkevproxy <kevinrodasuk@gmail.com>
2026-09-15 22:49:55 +0100

    Add initialization of second ArrayList in dynamicArray class

--- dynamicArray.java
+++ dynamicArray.java
@@ -5,8 +5,9 @@ import java.util.ArrayList;
 public class dynamicArray {
         public static void main(String[] args) {
             ArrayList<String> arrayList = new ArrayList<>(); 
+            ArrayList<String> arrayList2 = new ArrayList<>(); 
 
 
-            
+
         }    
 }
