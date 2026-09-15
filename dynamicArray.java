f59e25ed2b61cb204b0baa9d8426af051d2807db
rodkevproxy <kevinrodasuk@gmail.com>
2026-09-15 11:36:18 +0100

    Clean up formatting and remove unnecessary whitespace in dynamicArray class

--- dynamicArray.java
+++ dynamicArray.java
@@ -1,11 +1,10 @@
 //Static Array 
 
-
-
-
-
-
+import java.util.ArrayList;
 
 public class dynamicArray {
-    
+        public static void main(String[] args) {
+            ArrayList<String> arrayList = new ArrayList<>(); 
+            
+        }    
 }
