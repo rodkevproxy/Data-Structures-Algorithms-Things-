fadee33553c495cd8d4f85ec01f76d7ff8e3918a
rodkevproxy <kevinrodasuk@gmail.com>
2026-09-17 21:39:10 +0100

    Refactor dynamicArray class and add initial implementation of data structures and algorithms

--- dynamicArray.java
+++ dynamicArray.java
@@ -4,8 +4,20 @@ import java.util.ArrayList;
 
 public class dynamicArray {
         public static void main(String[] args) {
-            ArrayList<String> arrayList = new ArrayList<>(); 
-            ArrayList<String> arrayList2 = new ArrayList<>(); 
+            int capacity = 10; //This will be the initial capacity 
+            int size; 
+            Object[] array; //This is an array of objects 
+
+            //Overloaded constructoirs 
+            public DynamicArray(){ 
+                this.array = new Object[capcity];
+                this.capacity = new 
+            } 
+
+
+
+
+
             
 
 
